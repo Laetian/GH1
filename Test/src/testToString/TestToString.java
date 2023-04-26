389e57521dc6876e3848be4a01539920bc4b2da0
Laetian <laetianv@gmail.com>
2023-04-26 14:20:50 +0200

    Create TestToString.java

---
+++ Test/src/testToString/TestToString.java
@@ -0,0 +1,19 @@
+package testToString;
+
+public class TestToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Alumno alumno = new Alumno();
+		System.out.println(alumno.toString());
+		
+
+	}
+
+}
+
+
+class Alumno {
+	
+}
