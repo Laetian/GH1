2d9eeb903c8ddce4bdf9712a677907ee16ca82b7
Laetian <laetianv@gmail.com>
2023-04-26 14:30:03 +0200

    +metodos

---
+++ Test/src/metodosString/MetodoIndexOf.java
@@ -0,0 +1,14 @@
+package metodosString;
+
+public class MetodoIndexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String hola= new String("hola");
+		
+		System.out.println(hola.indexOf('l')); //Nos devuele la posición de lo que le metamos, en caso de no existir devuelve -1, empieza desde 0
+	}
+
+}
---
+++ Test/src/metodosString/MetodoLength.java
@@ -0,0 +1,19 @@
+package metodosString;
+
+public class MetodoLength {
+	
+	public static void main(String[] args) {
+		
+		
+		String hola=new String ("Hola");
+		
+		
+		System.out.println(hola.length());// devuelve la longitud de la cadena
+		
+		
+	}
+	
+
+
+
+}
