3c1e338651bb04f281d556850eecf78dbc87b6c0
Laetian <laetianv@gmail.com>
2023-04-26 14:23:51 +0200

    Create MetodoTrimm.java

---
+++ Test/src/metodosString/MetodoTrimm.java
@@ -0,0 +1,15 @@
+package metodosString;
+
+public class MetodoTrimm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String hola="  hola  ";
+		
+		System.out.println(hola); // devuelve "  hola  " con los espacios
+		System.out.println(hola.trim()); // devuelve "hola" , elimina los espacios iniciales y finales
+	}
+
+}
