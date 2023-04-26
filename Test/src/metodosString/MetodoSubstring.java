a9a6e59e438e0a2158d9adc077f9c4c87398dff9
Laetian <laetianv@gmail.com>
2023-04-26 14:33:17 +0200

    Create MetodoSubstring.java

---
+++ Test/src/metodosString/MetodoSubstring.java
@@ -0,0 +1,16 @@
+package metodosString;
+
+public class MetodoSubstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hola= new String("hola");
+		
+		System.out.println(hola.substring(1));//Nos da el substring desde donde empezamos hasta el final
+		
+		System.out.println(hola.substring(1, 2));//Nos da el substring desde donde empezamos hasta donde le hemos indicado que termine
+
+	}
+
+}
