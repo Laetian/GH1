a9cb49e40cffab3965c2c93aae335424f9770816
Laetian <laetianv@gmail.com>
2023-05-29 09:59:40 +0200

    Update ClaseAbstract.java

--- Test/src/claseAbstracta/ClaseAbstract.java
+++ Test/src/claseAbstracta/ClaseAbstract.java
@@ -1,15 +1,25 @@
 package claseAbstracta;
 
 public abstract class ClaseAbstract {
-	
+
 	public void metodoAbtracto() {
-		
+
 	}
+
 	public double metodoNoAbstractoParametros(int entero) {
 		return 0;
 	}
+
 	public abstract double metodoAbstractoParametros(int entero);
-	
-	
+
+}
+
+class HeredaClaseAbstract extends ClaseAbstract {
+
+	@Override
+	public double metodoAbstractoParametros(int entero) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
