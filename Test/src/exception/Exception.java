d0100861927dd18dfebac255e82bc9b32bc992e1
Laetian <laetianv@gmail.com>
2023-05-31 12:23:28 +0200

    Create Exception.java

---
+++ Test/src/exception/Exception.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package exception;
+
+/**
+ * @author Alumno
+ *
+ */
+public class Exception {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a, d;
+		d = 0;
+		a = 42 / d;
+
+		System.out.println("mensaje");
+	}
+
+}
